743a2dc3f0fb5dcaed2a9d4b91871a0a09d4c56b
kuntumsenda <kuntumchaeras@gmail.com>
2025-06-08 22:37:18 +0700

    wip: error json

--- json_response/json_response.go
+++ json_response/json_response.go
@@ -3,27 +3,29 @@ package json_response
 import "github.com/gin-gonic/gin"
 
 type APIResponse struct {
-	Code    string      `json:"code"`
-	Message string      `json:"message,omitempty"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   interface{} `json:"error,omitempty"`
+	CodeSystem   string      `json:"codeSystem"`
+	Code         string      `json:"code"`
+	Message      string      `json:"message,omitempty"`
+	MessageError string      `json:"messageError,omitempty"`
+	Result       interface{} `json:"data,omitempty"`
 }
 
-func Success(ctx *gin.Context, data interface{}) {
+func Success(ctx *gin.Context, codeSystem string, result interface{}) {
 	ctx.JSON(200, APIResponse{
-		Code:    "00",
-		Message: "success",
-		Data:    data,
+		CodeSystem: codeSystem,
+		Code:       "00",
+		Message:    "success",
+		Result:     result,
 	})
 	panic(nil)
 }
 
-func Error(ctx *gin.Context, httpCode int, code string, message string, err interface{}) {
+func Error(ctx *gin.Context, codeSystem string, httpCode int, code string, message string) {
 	ctx.JSON(httpCode, APIResponse{
-		Code:    code,
-		Message: message,
-		Data:    "",
-		Error:   err,
+		CodeSystem:   codeSystem,
+		Code:         code,
+		MessageError: message,
+		Result:       "",
 	})
 	panic(nil)
 }
