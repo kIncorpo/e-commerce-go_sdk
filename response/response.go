81b801112fa9c38057a99c04f4b05b7d81ec8dce
kuntumsenda <kuntumchaeras@gmail.com>
2025-06-08 19:53:08 +0700

    wip: base response

---
+++ response/response.go
@@ -0,0 +1,27 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+type APIResponse struct {
+	Code    string      `json:"code"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   interface{} `json:"error,omitempty"`
+}
+
+func Success(ctx *gin.Context, data interface{}) {
+	ctx.JSON(200, APIResponse{
+		Code:    "00",
+		Message: "success",
+		Data:    data,
+	})
+}
+
+func Error(ctx *gin.Context, httpCode int, code string, message string, err interface{}) {
+	ctx.JSON(httpCode, APIResponse{
+		Code:    code,
+		Message: message,
+		Data:    nil,
+		Error:   err,
+	})
+}
